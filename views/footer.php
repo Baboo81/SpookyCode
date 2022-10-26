8608394ca367ac034ede92f0dc5feb1c90b1c69c
Zahra Chawki <z.chawki@interface3.be>
2022-10-26 16:34:27 +0200

    début header-footer

---
+++ views/footer.php
@@ -0,0 +1,9 @@
+
+<footer>
+    
+    <div class="ul-footer">
+        <div class="image"><img src="../assets/img/logoI3.png" alt="" id="logo-I3"></div>
+        <p>Mentions légales</p>
+    </div>
+
+</footer>
---
+++ views/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/styles1.css">
+    <title>Header</title>
+</head>
+<body>
+    <header>
+        <img src="../assets/img/logo-green.png" alt=""id="logo">
+        <ul class="ul-header">
+            <li>Jeu</li>
+            <li>About us</li>
+        </ul>
+    </header>
+
+    <div class="container">
+
+    </div>
+
+    
+</body>
+</html>
