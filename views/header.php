9632086e75b591ac1d5eb67eb2fb39b0b1366979
Zahra Chawki <z.chawki@interface3.be>
2022-10-27 15:20:43 +0200

    reglage footer

--- views/header.php
+++ views/header.php
@@ -1,32 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/css/styles1.css">
-    <title>Header</title>
-</head>
-<body>
+
     <header>
-        <img src="../assets/img/logoSpooky.svg" alt=""id="logo">
+        <img src="./assets/img/logoSpooky.svg" alt=""id="logo">
         <ul class="ul-header">
-            <li>Jeu</li>
-            <li>About us</li>
+            <li class="jeu">Jeu</li>
+            <li>A propos de nous</li>
         </ul>
     </header>
-
-    <div class="container">
-
-
-    </div>
-
-    <footer>
-
-     <div class="image"><img src="../assets/img/logoI3.png" alt="" id="logo-I3"></div>
-
-     <p class="mentions">Mentions legales</p>
-
-    </footer>
-</body>
-</html>
