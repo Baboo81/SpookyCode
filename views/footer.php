42eb98a646ea97cad238d486e29a5aedd522f93e
Thais Sablon <sablon.thais@hotmail.com>
2022-10-27 21:23:55 +0200

    T_ajoutReset+ModifsHeaderFooter

--- views/footer.php
+++ views/footer.php
@@ -1,19 +1,20 @@
 
 <footer>
 
-<div class="image"><img src="./assets/img/logoI3.png" alt="" id="logo-I3">
-</div>
+    <div class="image">
+        <img src="./assets/img/logoI3.png" alt="Logo Interface3" id="logo-I3">
+        <p class="hack">Hackathon 2022 avec Interface3</p>
+    </div>
 
-<div class="text-footer">
-    <p class="hack">Hackathon 2022 par Interface3</p>
-    <p class="mentions">Mentions legales</p>
+    <div class="text-footer">
+        <p class="mentions">Mentions légales</p>
+    </div>
     <div class="social">
         <img class="icon" src="./assets/img/github.png">
         <img class="icon" src="./assets/img/linkedin.png">
         <img class="icon" src="./assets/img/facebook.png">
         <img class="icon" src="./assets/img/instagram.png">
-    </div>
-</div>
+    </div> 
 
 
 </footer>
