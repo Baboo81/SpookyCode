0770171f8a3bdc9f3f14f10bf85862958ce74238
Christel Rodriguez Perez <magicbaboo@gmail.com>
2022-10-28 10:58:52 +0200

    Added login.php

--- views/login.php
+++ views/login.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets/css/styles2.css"
-    <title></title>
-</head>
 
-<body>
     
         <form name="login" action="">
 
@@ -27,5 +17,4 @@
 
         </form>
     
-    </body>
-</html>
+ 
