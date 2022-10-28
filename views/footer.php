e4b47418d57ede6414bffbdfaf2a835a6ace0d5f
Zahra Chawki <z.chawki@interface3.be>
2022-10-28 10:25:08 +0200

    reglage footer

--- views/footer.php
+++ views/footer.php
@@ -2,13 +2,16 @@
 <footer>
 
     <div class="image">
+
         <img src="./assets/img/logoI3.png" alt="Logo Interface3" id="logo-I3">
-        <p class="hack">Hackathon 2022 avec Interface3</p>
+        
     </div>
 
     <div class="text-footer">
+        <p class="hack">Hackathon 2022 avec Interface3</p>
         <p class="mentions">Mentions légales</p>
     </div>
+    
     <div class="social">
         <img class="icon" src="./assets/img/github.png">
         <img class="icon" src="./assets/img/linkedin.png">
